c8bef202c0eac1a76ae31b39976aee37f76f00d8
Loldaniel123 <53963741+Loldaniel123@users.noreply.github.com>
2019-08-10 16:07:09 +0900

    거꾸로 출력하기 (코드업)

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[1000];
+
+    int n;
+    scanf("%d",&n);
+
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(int i=n-1; i>=0; i--)
+    {
+        printf("%d", a[i]);
+    }
+
+
+
+
+
+    return 0;
+}
